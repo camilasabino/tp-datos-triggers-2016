f6937a0aa31a8551c0e7cb8c62b71024716872b8
Camila Sabino <camila2sabino@gmail.com>
2016-10-30 16:13:44 -0300

    Subo parte Marcos.

--- gd_clinica_medica_frba/objetos_de_bd_Cami.sql
+++ gd_clinica_medica_frba/objetos_de_bd_Cami.sql
@@ -27,7 +27,7 @@ CREATE PROC LOS_TRIGGERS.ModificarRol (@nombre varchar(255), @nuevo_nombre varch
 
 	END;
 GO
-
+*/
 --- << Funcionalidades >> ---
 IF OBJECT_ID ('LOS_TRIGGERS.AgregarFuncionalidad') is not null DROP PROCEDURE LOS_TRIGGERS.AgregarFuncionalidad
 GO
@@ -37,14 +37,36 @@ CREATE PROC LOS_TRIGGERS.AgregarFuncionalidad (@nombre varchar(255)) AS
 	END;
 GO
 
-IF OBJECT_ID ('LOS_TRIGGERS.Agregar_Funcionalidad_Rol') is not null DROP PROCEDURE LOS_TRIGGERS.Agregar_Funcionalidad_Rol
+---lista de fcs de un rol---
+IF OBJECT_ID ('DB_MASTERS.FuncionesRoles') is not null DROP PROCEDURE DB_MASTERS.FuncionesRoles
 GO
-CREATE PROC LOS_TRIGGERS.Agregar_Funcionalidad_Rol (@rol varchar(255), @funcionalidad varchar(255)) AS
-	SET IDENTITY_INSERT LOS_TRIGGERS.Funcionalidad_Rol ON
-	GO
-	BEGIN
+CREATE PROC DB_MASTERS.FuncionesRoles (@rol varchar(50)) AS
+begin
+	declare @id_rol integer = (select id_rol from DB_MASTERS.Roles where nombre=@rol)
+	select id_fc from DB_MASTERS.Funciones_Roles where id_rol=@id_rol order by id_fc
+end
+go
 
-	END;
-	SET IDENTITY_INSERT LOS_TRIGGERS.Funcionalidad_Rol OFF
+---Actualizar FC's por ROL---
+IF OBJECT_ID ('LOS_TRIGGERS.AgregarFuncionalidadRol') is not null DROP PROCEDURE LOS_TRIGGERS.AgregarFuncionalidadRol
 GO
-*/
+CREATE PROC LOS_TRIGGERS.AgregarFuncionalidadRol (@func numeric(18,0), @rol varchar(255), @habilitado bit) AS
+BEGIN
+	if @rol='afiliado'
+	else if @rol='profesional'
+	else if @rol='administrador'
+
+
+	declare @id_rol numeric(18,0) = (select id_rol from DB_MASTERS.Roles where nombre=@rol)
+	if @habilitado = 1
+	begin
+		if (select funcionalidad from LOS_TRIGGERS.Funcionalidad_Rol where funcionalidad=@func and rol=@id_rol) is null
+			insert into DB_MASTERS.Funciones_Roles (id_fc, id_rol) values (@fc, @id_rol)
+	end
+	else
+	begin
+		if (select id_fc from DB_MASTERS.Funciones_Roles where id_fc=@fc and id_rol=@id_rol) is not null
+			delete from DB_MASTERS.Funciones_Roles where id_fc=@fc and id_rol=@id_rol
+	end
+END
+GO
