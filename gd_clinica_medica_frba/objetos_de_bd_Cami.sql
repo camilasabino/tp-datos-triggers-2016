59d0bc473a21380f6d8d2ce5fdc7883d23717ea7
Camila Sabino <camila2sabino@gmail.com>
2016-10-30 16:51:41 -0300

    Alta rol.

--- gd_clinica_medica_frba/objetos_de_bd_Cami.sql
+++ gd_clinica_medica_frba/objetos_de_bd_Cami.sql
@@ -6,67 +6,81 @@
 /*
 IF OBJECT_ID ('LOS_TRIGGERS.AltaRol') is not null DROP PROCEDURE LOS_TRIGGERS.AltaRol
 GO
-CREATE PROC LOS_TRIGGERS.AltaRol (@nombre varchar(255)) AS
+CREATE PROC LOS_TRIGGERS.AltaRol (@nombre_rol varchar(255)) AS
 	BEGIN
-		
-	END;
-GO
-
-IF OBJECT_ID ('LOS_TRIGGERS.BajaRol') is not null DROP PROCEDURE LOS_TRIGGERS.BajaRol
-GO
-CREATE PROC LOS_TRIGGERS.BajaRol (@nombre varchar(255)) AS
-	BEGIN
-
+	
 	END;
 GO
 
 IF OBJECT_ID ('LOS_TRIGGERS.ModificarRol') is not null DROP PROCEDURE LOS_TRIGGERS.ModificarRol
 GO
-CREATE PROC LOS_TRIGGERS.ModificarRol (@nombre varchar(255), @nuevo_nombre varchar(255)) AS
+CREATE PROC LOS_TRIGGERS.ModificarRol (@nombre varchar(255)) AS
 	BEGIN
 
 	END;
 GO
 */
---- << Funcionalidades >> ---
-IF OBJECT_ID ('LOS_TRIGGERS.AgregarFuncionalidad') is not null DROP PROCEDURE LOS_TRIGGERS.AgregarFuncionalidad
+
+IF OBJECT_ID ('LOS_TRIGGERS.InhabilitarRol') is not null DROP PROCEDURE LOS_TRIGGERS.InhabilitarRol
 GO
-CREATE PROC LOS_TRIGGERS.AgregarFuncionalidad (@nombre varchar(255)) AS 
+CREATE PROC LOS_TRIGGERS.InhabilitarRol (@nombre_rol varchar(255)) AS
 	BEGIN
-		insert into LOS_TRIGGERS.Funcionalidad(func_nombre) values (@nombre)
+		IF (@nombre_rol='Afiliado')
+			begin
+				UPDATE LOS_TRIGGERS.Afiliado set afil_habilitacion = 0
+				UPDATE LOS_TRIGGERS.Usuario set user_afiliado = null
+				where user_afiliado is not null
+			end
+		ELSE IF (@nombre_rol='Administrador')
+			begin
+				UPDATE LOS_TRIGGERS.Administrador set admi_habilitacion = 0
+				UPDATE LOS_TRIGGERS.Usuario set user_administrador = null
+				where user_administrador is not null
+			end
+		ELSE IF (@nombre_rol='Profesional')
+			begin
+				UPDATE LOS_TRIGGERS.Profesional set prof_habilitacion = 0
+				UPDATE LOS_TRIGGERS.Usuario set user_profesional = null
+				where user_profesional is not null
+			end
 	END;
 GO
 
----lista de fcs de un rol---
-IF OBJECT_ID ('DB_MASTERS.FuncionesRoles') is not null DROP PROCEDURE DB_MASTERS.FuncionesRoles
+IF OBJECT_ID ('LOS_TRIGGERS.HabilitarRol') is not null DROP PROCEDURE LOS_TRIGGERS.HabilitarRol
 GO
-CREATE PROC DB_MASTERS.FuncionesRoles (@rol varchar(50)) AS
-begin
-	declare @id_rol integer = (select id_rol from DB_MASTERS.Roles where nombre=@rol)
-	select id_fc from DB_MASTERS.Funciones_Roles where id_rol=@id_rol order by id_fc
-end
-go
+CREATE PROC LOS_TRIGGERS.HabilitarRol (@nombre_rol varchar(255)) AS
+	BEGIN
+		IF (@nombre_rol='Afiliado')
+			UPDATE LOS_TRIGGERS.Afiliado set afil_habilitacion = 1
+		ELSE IF (@nombre_rol='Administrador')
+			UPDATE LOS_TRIGGERS.Administrador set admi_habilitacion = 1
+		ELSE IF (@nombre_rol='Profesional')
+			UPDATE LOS_TRIGGERS.Profesional set prof_habilitacion = 1
+	END;
+GO
 
----Actualizar FC's por ROL---
+
+--- << Funcionalidades >> ---
 IF OBJECT_ID ('LOS_TRIGGERS.AgregarFuncionalidadRol') is not null DROP PROCEDURE LOS_TRIGGERS.AgregarFuncionalidadRol
 GO
-CREATE PROC LOS_TRIGGERS.AgregarFuncionalidadRol (@func numeric(18,0), @rol varchar(255), @habilitado bit) AS
+CREATE PROC LOS_TRIGGERS.AgregarFuncionalidadRol (@funcionalidad varchar(255), @rol varchar(255)) AS
 BEGIN
-	if @rol='afiliado'
-	else if @rol='profesional'
-	else if @rol='administrador'
-
-
-	declare @id_rol numeric(18,0) = (select id_rol from DB_MASTERS.Roles where nombre=@rol)
-	if @habilitado = 1
-	begin
-		if (select funcionalidad from LOS_TRIGGERS.Funcionalidad_Rol where funcionalidad=@func and rol=@id_rol) is null
-			insert into DB_MASTERS.Funciones_Roles (id_fc, id_rol) values (@fc, @id_rol)
-	end
-	else
-	begin
-		if (select id_fc from DB_MASTERS.Funciones_Roles where id_fc=@fc and id_rol=@id_rol) is not null
-			delete from DB_MASTERS.Funciones_Roles where id_fc=@fc and id_rol=@id_rol
-	end
+		declare @func_id as numeric(18,0)
+		set @func_id = (select func_id from LOS_TRIGGERS.Funcionalidad where func_nombre=@funcionalidad)
+		IF (@rol='Afiliado')
+			begin
+				insert into LOS_TRIGGERS.Funcionalidad_Rol
+					select @func_id, afil_numero from LOS_TRIGGERS.Afiliado
+			end
+		ELSE IF (@rol='Administrador')
+			begin
+				insert into LOS_TRIGGERS.Funcionalidad_Rol
+					select @func_id, admi_id from LOS_TRIGGERS.Administrador
+			end
+		ELSE IF (@rol='Profesional')
+			begin
+				insert into LOS_TRIGGERS.Funcionalidad_Rol
+					select @func_id, prof_id from LOS_TRIGGERS.Profesional
+			end
 END
 GO
