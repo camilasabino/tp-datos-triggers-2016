f2ef3eb1d9965a1e1e2ab49bc5f783cb0af68185
Camila Sabino <camila2sabino@gmail.com>
2016-10-30 22:59:39 -0300

    Cambio algunas cosas del modelo. Alta y baja de Turnos.

--- gd_clinica_medica_frba/objetos_de_bd_Cami.sql
+++ gd_clinica_medica_frba/objetos_de_bd_Cami.sql
@@ -2,7 +2,7 @@
 *				           OBJETOS DE BASE DE DATOS                                  *
 **************************************************************************************/
 
---- << ABM ROLES >> ---
+--- << ABM de Rol >> ---
 /*
 IF OBJECT_ID ('LOS_TRIGGERS.AltaRol') is not null DROP PROCEDURE LOS_TRIGGERS.AltaRol
 GO
@@ -26,23 +26,20 @@ GO
 CREATE PROC LOS_TRIGGERS.InhabilitarRol (@nombre_rol varchar(255)) AS
 	BEGIN
 		IF (@nombre_rol='Afiliado')
-			begin
+			BEGIN
 				UPDATE LOS_TRIGGERS.Afiliado set afil_habilitacion = 0
-				UPDATE LOS_TRIGGERS.Usuario set user_afiliado = null
-				where user_afiliado is not null
-			end
+				UPDATE LOS_TRIGGERS.Usuario set user_afiliado = null where user_afiliado is not null
+			END
 		ELSE IF (@nombre_rol='Administrador')
-			begin
+			BEGIN
 				UPDATE LOS_TRIGGERS.Administrador set admi_habilitacion = 0
-				UPDATE LOS_TRIGGERS.Usuario set user_administrador = null
-				where user_administrador is not null
-			end
+				UPDATE LOS_TRIGGERS.Usuario set user_administrador = null where user_administrador is not null
+			END
 		ELSE IF (@nombre_rol='Profesional')
-			begin
+			BEGIN
 				UPDATE LOS_TRIGGERS.Profesional set prof_habilitacion = 0
-				UPDATE LOS_TRIGGERS.Usuario set user_profesional = null
-				where user_profesional is not null
-			end
+				UPDATE LOS_TRIGGERS.Usuario set user_profesional = null where user_profesional is not null
+			END
 	END;
 GO
 
@@ -51,11 +48,11 @@ GO
 CREATE PROC LOS_TRIGGERS.HabilitarRol (@nombre_rol varchar(255)) AS
 	BEGIN
 		IF (@nombre_rol='Afiliado')
-			UPDATE LOS_TRIGGERS.Afiliado set afil_habilitacion = 1
+			update LOS_TRIGGERS.Afiliado set afil_habilitacion = 1
 		ELSE IF (@nombre_rol='Administrador')
-			UPDATE LOS_TRIGGERS.Administrador set admi_habilitacion = 1
+			update LOS_TRIGGERS.Administrador set admi_habilitacion = 1
 		ELSE IF (@nombre_rol='Profesional')
-			UPDATE LOS_TRIGGERS.Profesional set prof_habilitacion = 1
+			update LOS_TRIGGERS.Profesional set prof_habilitacion = 1
 	END;
 GO
 
@@ -68,19 +65,72 @@ BEGIN
 		declare @func_id as numeric(18,0)
 		set @func_id = (select func_id from LOS_TRIGGERS.Funcionalidad where func_nombre=@funcionalidad)
 		IF (@rol='Afiliado')
-			begin
+			BEGIN
 				insert into LOS_TRIGGERS.Funcionalidad_Rol
 					select @func_id, afil_numero from LOS_TRIGGERS.Afiliado
-			end
+			END
 		ELSE IF (@rol='Administrador')
-			begin
+			BEGIN
 				insert into LOS_TRIGGERS.Funcionalidad_Rol
 					select @func_id, admi_id from LOS_TRIGGERS.Administrador
-			end
+			END
 		ELSE IF (@rol='Profesional')
-			begin
+			BEGIN
 				insert into LOS_TRIGGERS.Funcionalidad_Rol
 					select @func_id, prof_id from LOS_TRIGGERS.Profesional
-			end
+			END
+END
+GO
+
+IF OBJECT_ID ('LOS_TRIGGERS.QuitarFuncionalidadRol') is not null DROP PROCEDURE LOS_TRIGGERS.QuitarFuncionalidadRol
+GO
+CREATE PROC LOS_TRIGGERS.QuitarFuncionalidadRol (@funcionalidad varchar(255), @rol varchar(255)) AS
+BEGIN
+		declare @func_id as numeric(18,0)
+		set @func_id = (select func_id from LOS_TRIGGERS.Funcionalidad where func_nombre=@funcionalidad)
+		IF (@rol='Afiliado')
+			BEGIN
+				delete from LOS_TRIGGERS.Funcionalidad_Rol
+				where funcionalidad=@func_id AND rol IN (select afil_numero from LOS_TRIGGERS.Afiliado)
+			END
+		ELSE IF (@rol='Administrador')
+			BEGIN
+				delete from LOS_TRIGGERS.Funcionalidad_Rol
+				where funcionalidad=@func_id AND rol IN (select admi_id from LOS_TRIGGERS.Administrador)
+			END
+		ELSE IF (@rol='Profesional')
+			BEGIN
+				delete from LOS_TRIGGERS.Funcionalidad_Rol
+				where funcionalidad=@func_id AND rol IN (select prof_id from LOS_TRIGGERS.Profesional)
+			END
+END
+GO
+
+--- << Pedido y Cancelación de Turno >> ---
+IF OBJECT_ID ('LOS_TRIGGERS.RegistrarTurno') is not null DROP PROCEDURE LOS_TRIGGERS.RegistrarTurno
+GO
+CREATE PROC LOS_TRIGGERS.CancelarTurno (@afiliado numeric(18,00), @profesional numeric(18,00), @fecha datetime, @motivo varchar(255)) AS
+BEGIN
+		insert into LOS_TRIGGERS.Turno (turn_afiliado, turn_profesional, turn_fecha_atencion)
+			values(@afiliado, @profesional, @fecha)
+END
+GO
+
+IF OBJECT_ID ('LOS_TRIGGERS.CancelarTurno') is not null DROP PROCEDURE LOS_TRIGGERS.CancelarTurno
+GO
+CREATE PROC LOS_TRIGGERS.CancelarTurno (@afiliado numeric(18,00), @profesional numeric(18,00), @turno numeric(18,0), @tipo numeric(18,0), @motivo varchar(255)) AS
+BEGIN
+	declare @fecha_turno as datetime
+	set @fecha_turno = (select turn_fecha_atencion from LOS_TRIGGERS.Turno where turn_numero=@turno)
+
+	IF (GETDATE() < @fecha_turno)
+		BEGIN
+			insert into LOS_TRIGGERS.Cancelacion_Turno (canc_afiliado, canc_profesional, canc_fecha, canc_fecha_turno, canc_tipo, canc_motivo, can_horario_atencion)
+				select @afiliado, @profesional, @fecha_turno, GETDATE(), @tipo, @motivo, hora_id
+				from LOS_TRIGGERS.Horario_Atencion where hora_turno=@turno
+
+			update LOS_TRIGGERS.Horario_Atencion set hora_turno = null where hora_turno=@turno
+			delete from LOS_TRIGGERS.Turno where turn_numero=@turno
+		END
 END
 GO
