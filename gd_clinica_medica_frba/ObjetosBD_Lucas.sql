51145a851303c446e5b0646216154a7cecfbe6dc
LucasGiani05 <LucasGiani_05@outlook.com.ar>
2016-11-04 17:51:08 -0300

    Correcciones y agregados en ABM Afiliado

--- gd_clinica_medica_frba/ObjetosBD_Lucas.sql
+++ gd_clinica_medica_frba/ObjetosBD_Lucas.sql
@@ -1,6 +1,5 @@
 /******** ABM Afiliados *********/
 
-
 --<Modificación de los datos del Afiliado>--
 
 IF OBJECT_ID ('LOS_TRIGGERS.ModificarAfiliadoDireccion') is not null DROP PROCEDURE LOS_TRIGGERS.ModificarAfiliadoDireccion
@@ -29,16 +28,29 @@ GO
 
 IF OBJECT_ID ('LOS_TRIGGERS.ModificarAfiliadoPlanMedico') is not null DROP PROCEDURE LOS_TRIGGERS.ModificarAfiliadoPlanMedico
 GO
-CREATE PROC LOS_TRIGGERS.ModificarAfiliadoPlanMedico(@afiliado numeric(18,0), @nuevo_plan numeric(18,0), @motivo varchar(255)) AS
+CREATE PROC LOS_TRIGGERS.ModificarAfiliadoPlanMedico(@afiliado numeric(18,0), @viejo_plan numeric(18,0), @nuevo_plan numeric(18,0), @motivo varchar(255)) AS
+	SET IDENTITY_INSERT LOS_TRIGGERS.Afiliado ON
 	BEGIN
-		DECLARE @fecha datetime
-		update LOS_TRIGGERS.Afiliado set afil_plan_medico = @nuevo_plan where afil_numero = @afiliado
-		insert into LOS_TRIGGERS.Modificacion_Plan(modi_id,modi_fecha_y_hora,modi_motivo) 
-				values (@nuevo_plan, GETDATE(), @motivo)
+		insert into LOS_TRIGGERS.Modificacion_Plan(modi_afiliado, modi_viejo_plan, modi_nuevo_plan, modi_fecha_y_hora, modi_motivo) 
+				values (@afiliado, @viejo_plan, @nuevo_plan, GETDATE(), @motivo)
+		IF (@afiliado = (select afil_titular_grupo_familiar from LOS_TRIGGERS.Afiliado where afil_numero=@afiliado))
+			update LOS_TRIGGERS.Afiliado set afil_plan_medico = @nuevo_plan where afil_titular_grupo_familiar = @afiliado
+		ELSE
+			update LOS_TRIGGERS.Afiliado set afil_plan_medico = @nuevo_plan where afil_numero = @afiliado
+	SET IDENTITY_INSERT LOS_TRIGGERS.Afiliado OFF
+	END;
+GO
+
+IF OBJECT_ID ('LOS_TRIGGERS.ModificarAfiliadoEstadoCivil') is not null DROP PROCEDURE LOS_TRIGGERS.ModificarAfiliadoEstadoCivil
+GO
+CREATE PROC LOS_TRIGGERS.ModificarAfiliadoEstadoCivil(@afiliado numeric(18,0), @nuevoEstadoCivil varchar(255)) AS
+	BEGIN
+		update LOS_TRIGGERS.Afiliado set afil_estado_civil = @nuevoEstadoCivil where afil_numero = @afiliado
 	END;
 GO
 
 --<Baja Afiliado>--
+
 IF OBJECT_ID ('LOS_TRIGGERS.RegistrarBajaLogicaAfiliado') is not null DROP TRIGGER LOS_TRIGGERS.RegistrarBajaLogicaAfiliado
 GO
 CREATE TRIGGER LOS_TRIGGERS.RegistrarBajaLogicaAfiliado ON LOS_TRIGGERS.Afiliado 
@@ -60,39 +72,35 @@ CREATE PROC LOS_TRIGGERS.DarDeBajaUnAfiliado(@afiliado numeric(18,0)) AS
 	END;
 GO
 
-
-
 --<Alta Afiliado>--
 
 IF OBJECT_ID ('LOS_TRIGGERS.CalcularNumeroAfiliado') is not null DROP FUNCTION LOS_TRIGGERS.CalcularNumeroAfiliado
 GO
-CREATE FUNCTION LOS_TRIGGERS.CalcularNumeroAfiliado(@afiliado numeric(18,0), @titular numeric(18,0), @relacionConTitular varchar(255))
+CREATE FUNCTION LOS_TRIGGERS.CalcularNumeroAfiliado(@dni numeric(18,0), @numeroDelTitular numeric(18,0), @relacionConTitular varchar(255))
 	RETURNS numeric(18,0)
 	AS
 	BEGIN
-		IF(@afiliado = @titular) RETURN @afiliado
-		ELSE  DECLARE @numeroAfiliado numeric(18,0)
-		IF(@relacionConTitular = 'Conyuge') 
-			BEGIN	
-				set @numeroAfiliado = @afiliado + 1
-				RETURN @numeroAfiliado
-			END
-		ELSE IF(@relacionConTitular = 'Hijo' OR @relacionConTitular = 'Hija')
-			BEGIN
-				set @numeroAfiliado = (select TOP 1 afil_numero from Afiliado where afil_titular_grupo_familiar = @titular order by afil_numero DESC) + 1
-				RETURN @numeroAfiliado
-			END
-			RETURN @numeroAfiliado
+		DECLARE @numeroAfiliado numeric(18,0) 
+		IF(@relacionConTitular = 'Titular') set @numeroAfiliado = (@dni*100) + 1  
+		ELSE IF(@relacionConTitular = 'Cónyuge') set @numeroAfiliado = @numeroDelTitular + 1
+		ELSE IF(@relacionConTitular = 'Hijo/a') set @numeroAfiliado = (select TOP 1 afil_numero from LOS_TRIGGERS.Afiliado where afil_titular_grupo_familiar = @numeroDelTitular order by afil_numero DESC) + 1
+		RETURN @numeroAfiliado
 	END
 GO
 
 
 IF OBJECT_ID ('LOS_TRIGGERS.DarDeAltaUnAfiliado') is not null DROP PROCEDURE LOS_TRIGGERS.DarDeAltaUnAfiliado
 GO
-CREATE PROC LOS_TRIGGERS.DarDeAltaUnAfiliado(@afiliadoNumero numeric(18,0),@estadoCivil varchar(255), @titular numeric(18,0), @plan numeric(18,0), @familiaresACargo numeric(2,0), @relacionConTitular varchar(255)) AS
+CREATE PROC LOS_TRIGGERS.DarDeAltaUnAfiliado(@dni numeric(18,0), @titularNumero numeric(18,0), @estadoCivil varchar(255), @plan numeric(18,0), @familiaresACargo numeric(2,0), @relacionConTitular varchar(255)) AS
+	SET IDENTITY_INSERT LOS_TRIGGERS.Afiliado ON
 	BEGIN
 		insert into LOS_TRIGGERS.Afiliado(afil_numero, afil_estado_civil, afil_habilitacion, nombre_rol, afil_plan_medico, afil_cant_consultas_realizadas, afil_cant_familiares_a_cargo, afil_relacion_con_titular) 
-			values (LOS_TRIGGERS.CalcularNumeroAfiliado(@afiliadoNumero, @titular,@relacionConTitular), @estadoCivil, 1, 'Afiliado', @plan, 0, @familiaresACargo, @relacionConTitular) 
+			values (LOS_TRIGGERS.CalcularNumeroAfiliado(@dni, @titularNumero,@relacionConTitular), @estadoCivil, 1, 'Afiliado', @plan, 0, @familiaresACargo, @relacionConTitular) 
+		IF(@relacionConTitular = 'Hijo/a')
+		BEGIN
+			update LOS_TRIGGERS.Afiliado set afil_cant_familiares_a_cargo = afil_cant_familiares_a_cargo + 1 where afil_numero = @titularNumero
+		END
+	SET IDENTITY_INSERT LOS_TRIGGERS.Afiliado OFF
 	END;
 
 GO
